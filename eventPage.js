28865833a68fcb47f1e70765b288d61be7c71bba
Max Winde <max.winde@gmail.com>
2013-03-15 18:37:08 +0100

    creating a list of tabs to hibernate

--- eventPage.js
+++ eventPage.js
@@ -3,6 +3,15 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   console.dir(tab);
 
   chrome.windows.getAll({ populate: true}, function(windows) {
-    console.dir(windows);
+    _.each(windows, function(window) {
+      _.each(window.tabs, function(tab) {
+        if (tab.active) return;
+        if (tab.status != 'complete') return;
+        if (!tab.url.match(/^https?:\/\//)) return;
+
+        console.log('hibernate ' + tab.url);
+        console.dir(tab);
+      });
+    })
   });
 });
