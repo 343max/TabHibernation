4a3783733d8443b28619b0cdadd1d30480169799
Max Winde <max.winde@gmail.com>
2013-03-15 19:22:04 +0100

    showing the correct favicon

--- hibernationPage/js/main.js
+++ hibernationPage/js/main.js
@@ -1,7 +1,16 @@
+function setFavicon(faviconHref) {
+  var link = document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = faviconHref;
+  document.getElementsByTagName('head')[0].appendChild(link);
+}
+
 window.onload = function() {
   var json = decodeURIComponent(document.location.hash.substr(1, 100000));
   var pageInfo = JSON.parse(json);
   console.dir(pageInfo);
 
   document.title = pageInfo.title;
+  setFavicon(pageInfo.favIconUrl);
 }
