4367a03cc2fa86267982cb59d81bccfdb3abbd1a
Max Winde <max.winde@gmail.com>
2013-03-15 23:17:42 +0100

    don't kill all pages at once but one after another.

--- eventPage.js
+++ eventPage.js
@@ -2,26 +2,30 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   console.log('click!');
   console.dir(tab);
 
+  var c = 0;
+
   chrome.windows.getAll({ populate: true}, function(windows) {
-    _.each(windows, function(window) {
-      _.each(window.tabs, function(tab) {
+    _.each(windows, function(win) {
+      _.each(win.tabs, function(tab) {
         if (tab.active) return;
         if (tab.status != 'complete') return;
         if (!tab.url.match(/^https?:\/\//)) return;
 
-        console.log('hibernate ' + tab.url);
-        console.dir(tab);
+        window.setTimeout(function() {
 
-        var pageInfo = {
-          url: tab.url,
-          title: tab.title,
-          favIconUrl: tab.favIconUrl
-        };
+          var pageInfo = {
+            url: tab.url,
+            title: tab.title,
+            favIconUrl: tab.favIconUrl
+          };
 
-        var hibernationUrl = chrome.extension.getURL('hibernationPage/index.html');
-        hibernationUrl += '#' + encodeURIComponent(JSON.stringify(pageInfo));
-        console.log(hibernationUrl);
-        chrome.tabs.update(tab.id, {url: hibernationUrl});
+          var hibernationUrl = chrome.extension.getURL('hibernationPage/index.html');
+          hibernationUrl += '#' + encodeURIComponent(JSON.stringify(pageInfo));
+          console.log(hibernationUrl);
+          chrome.tabs.update(tab.id, {url: hibernationUrl});
+        }, c * 100);
+        c++;
+
       });
     })
   });
