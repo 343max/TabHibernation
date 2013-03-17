26416d29fb1688e55632ff0b1268828a2f386627
Max Winde <max.winde@gmail.com>
2013-03-17 12:54:54 +0100

    everything is now in a data URI

--- eventPage.js
+++ eventPage.js
@@ -9,41 +9,33 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   xmlHttp.onreadystatechange = function () {
     console.dir(xmlHttp);
     if (xmlHttp.readyState == 4) {
-//      alert(xmlHttp.responseText);
+      var html = xmlHttp.responseText;
+
+      chrome.windows.getAll({ populate: true}, function(windows) {
+        _.each(windows, function(win) {
+          _.each(win.tabs, function(tab) {
+            if (tab.active) return;
+            if (tab.status != 'complete') return;
+            if (!tab.url.match(/^https?:\/\//)) return;
+
+            window.setTimeout(function() {
+
+              var pageInfo = {
+                url: tab.url,
+                title: tab.title,
+                favIconUrl: tab.favIconUrl
+              };
+
+              var pageHtml = html.replace(/\{\/\*pageInfoObject\*\/\}/, JSON.stringify(pageInfo));
+              var dataURL = 'data:text/html;charset=utf-8,' + encodeURIComponent(pageHtml);
+              chrome.tabs.update(tab.id, {url: dataURL});
+            }, c * 100);
+            c++;
+
+          });
+        })
+      });
     }
   };
   xmlHttp.send(null);
-
-  chrome.windows.getAll({ populate: true}, function(windows) {
-    _.each(windows, function(win) {
-      _.each(win.tabs, function(tab) {
-        if (tab.active) return;
-        if (tab.status != 'complete') return;
-        if (!tab.url.match(/^https?:\/\//)) return;
-
-        window.setTimeout(function() {
-
-          var pageInfo = {
-            url: tab.url,
-            title: tab.title,
-            favIconUrl: tab.favIconUrl
-          };
-
-          var hibernationUrl = chrome.extension.getURL('hibernationPage/index.html');
-          hibernationUrl += '#' + encodeURIComponent(JSON.stringify(pageInfo));
-//          hibernationUrl = 'data:text/html;charset=utf-8,' +
-//            encodeURIComponent( // Escape for URL formatting
-//              '<!DOCTYPE html>'+
-//                '<html lang="en">'+
-//                '<head><title>Embedded Window</title></head>'+
-//                '<body><h1>42</h1></body>'+
-//                '</html>'
-//            );
-          chrome.tabs.update(tab.id, {url: hibernationUrl});
-        }, c * 100);
-        c++;
-
-      });
-    })
-  });
 });
