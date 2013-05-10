711e4cc8559c09d6058175aa926872bc685a86ae
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2013-05-10 16:28:38 +0200

    Replicate Chrome’s eventPage.js in Safari

--- TabHibernation.safariextension/global-script.js
+++ TabHibernation.safariextension/global-script.js
@@ -1,1 +1,36 @@
+var performCommand = function(event) {
+	if(event.command === 'hibernateTabs') {
+		var c = 0;
 
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open('GET', safari.extension.baseURI + 'lib/hibernationPage/index.html', true);
+		xmlHttp.onreadystatechange = function() {
+			if(xmlHttp.readyState === 4) {
+				var html = xmlHttp.responseText;
+
+				_.each(safari.application.browserWindows, function(win) {
+					_.each(win.tabs, function(tab) {
+						if(tab === win.activeTab) return;
+						if(!tab.url.match(/^https?:\/\//)) return;
+
+						_.delay(function() {
+							var pageInfo = {
+								url: tab.url,
+								title: tab.title
+							};
+
+							var pageHtml = html.replace(/\{\/\*pageInfoObject\*\/\}/, JSON.stringify(pageInfo));
+							var dataURL = 'data:text/html;charset=utf-8,' + encodeURIComponent(pageHtml);
+							tab.url = dataURL;
+						}, c * 100);
+
+						c++;
+					});
+				});
+			}
+		};
+		xmlHttp.send(null);
+	}
+};
+
+safari.application.addEventListener('command', performCommand, false);
