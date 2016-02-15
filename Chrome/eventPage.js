ecd12c87875bd9db8f1940e4b3387040ff6a3b08
Thomas Sänger <HorayNarea@gmail.com>
2016-02-15 01:50:10 +0100

    unified formatting

--- Chrome/eventPage.js
+++ Chrome/eventPage.js
@@ -1,53 +1,56 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  var c = 0;
+	var c = 0;
 
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open('GET', chrome.extension.getURL('lib/hibernationPage/index.html'), true);
-  xmlHttp.onreadystatechange = function () {
-    if (xmlHttp.readyState == 4) {
-      var html = xmlHttp.responseText;
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open('GET', chrome.extension.getURL('lib/hibernationPage/index.html'), true);
+	xmlHttp.onreadystatechange = function () {
+		if (xmlHttp.readyState == 4) {
+			var html = xmlHttp.responseText;
 
-      chrome.windows.getAll({ populate: true}, function(windows) {
-        windows.forEach(function(win) {
-          win.tabs.forEach(function(tab) {
-      			if (tab.active) return;
-      			if (tab.status != 'complete') return;
-      			if (!tab.url.match(/^https?:\/\//)) return;
+			chrome.windows.getAll({ populate: true}, function(windows) {
+				windows.forEach(function(win) {
+					win.tabs.forEach(function(tab) {
+						if (tab.active) return;
+						if (tab.status != 'complete') return;
+						if (!tab.url.match(/^https?:\/\//)) return;
 
-            sleepTab(html, tab, c * 100);
-            c++;
-          });
-        })
-      });
-    }
-  };
-  xmlHttp.send(null);
+						sleepTab(html, tab, c * 100);
+						c++;
+					});
+				})
+			});
+		}
+	};
+	xmlHttp.send(null);
 });
+
 function sleepTab(html, tab, timeout) {
 	window.setTimeout(function() {
-	  var pageInfo = {
-		url: tab.url,
-		title: tab.title,
-		favIconUrl: tab.favIconUrl
-	  };
-	  var pageHtml = html.replace(/\{\/\*pageInfoObject\*\/\}/, JSON.stringify(pageInfo));
-	  var dataURL = 'data:text/html;charset=utf-8,' + encodeURIComponent(pageHtml);
-	  chrome.tabs.update(tab.id, {url: dataURL});
+		var pageInfo = {
+			url: tab.url,
+			title: tab.title,
+			favIconUrl: tab.favIconUrl
+		};
+		var pageHtml = html.replace(/\{\/\*pageInfoObject\*\/\}/, JSON.stringify(pageInfo));
+		var dataURL = 'data:text/html;charset=utf-8,' + encodeURIComponent(pageHtml);
+		chrome.tabs.update(tab.id, {url: dataURL});
 	}, timeout);
 }
+
 chrome.contextMenus.create({
 	id : "SleepTab" ,
 	title : "Hibernate This Page" ,
 	contexts : ["page"]
 });
+
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open('GET', chrome.extension.getURL('lib/hibernationPage/index.html'), true);
-  xmlHttp.onreadystatechange = function () {
-	if (xmlHttp.readyState == 4) {
-	  var html = xmlHttp.responseText;
-	  sleepTab(html, tab, 100);
-	}
-  };
-  xmlHttp.send(null);
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open('GET', chrome.extension.getURL('lib/hibernationPage/index.html'), true);
+	xmlHttp.onreadystatechange = function () {
+		if (xmlHttp.readyState == 4) {
+			var html = xmlHttp.responseText;
+			sleepTab(html, tab, 100);
+		}
+	};
+	xmlHttp.send(null);
 });
